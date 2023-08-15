799a86e7e0a98ba67064644865733e8306fb2131
Mikolaj Dolgoruki <Mikolaj.Dolgoruki@students.ittralee.ie>
2023-08-15 20:54:03 +0100

    Signed-off-by: Mikolaj Dolgoruki <Mikolaj.Dolgoruki@students.ittralee.ie>

---
+++ T00225834 MIKOLAJ DOLGORUKI/Assets/Scripts/Player Controller.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+
+    Rigidbody rb;
+
+    float xInput;
+    float yInput;
+
+    int score = 0;
+    public int winScore;
+
+    public GameObject winText;
+
+
+
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.y < -5f)
+        {
+            SceneManager.LoadScene("Game");
+        }
+    }
+
+ private void FixedUpdate()
+ {
+    xInput = Input.GetAxis("Horizontal");
+    yInput = Input.GetAxis("Vertical");
+
+
+    rb.AddForce(xInput * speed, 0, yInput* speed);
+
+ }
+ 
+ private void OnTriggerEnter(Collider other)
+{
+    if(other.gameObject.tag == "Coin")
+    {
+        other.gameObject.SetActive(false);
+
+        score++;
+        
+        if(score >= winScore)
+        {
+            //gamewin
+            winText.SetActive(true);            
+           
+        }
+        
+    }
+
+        
+
+ } 
+
+
+}
